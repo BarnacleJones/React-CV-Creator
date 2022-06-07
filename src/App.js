a07bce4de13b8074f60bca12334172889d0e4e67
BarnacleJones <benmjones@protonmail.com>
2022-06-07 16:17:26 +1200

    general components sorted

--- src/App.js
+++ src/App.js
@@ -5,18 +5,20 @@ import General from './components/General';
 class App extends React.Component {
 
   constructor() {
+    //inherits all properties of react.component?
     super();
-
+    //set properties of state
     this.state = {
-        name: '',
-        email: '',
-        phone: '',
-        linkedin: '',
-        github: '' ,            
+        name: ['', true],
+        email: ['', true],
+        phone: ['', true],
+        linkedin: ['', true],
+        github: ['', true],            
       } 
   }
 
   //Generic onSubmit function for <General> components
+  //used this below to work out how to use variables for setState
   //https://stackoverflow.com/questions/29280445/reactjs-setstate-with-a-dynamic-key-name
   onSubmit(e)
   {
@@ -26,26 +28,31 @@ class App extends React.Component {
     //get value of the label (made lower case, uppers dont work?)
     var item = e.target.children[0].firstChild.nodeValue.toLowerCase();
     //use these values to create an object, updating the state 
-    this.setState({[item]: value});
-  }
-
-
+    if(this.state[item][1])
+    {
+    this.setState({[item]: [value, false]});
+    }
+    else{this.setState({[item]: [value, true]});   
+  }}
 
   render()
-  {
-    return(<div className="App">
+  {return(<div className="App">
       <h1>CV App</h1>
+      <div>
       {/* Form area */}
-      <General field="Name" onSub={this.onSubmit.bind(this)} />
-      <General field="Email" onSub={this.onSubmit.bind(this)} />
-      <General field="Phone" onSub={this.onSubmit.bind(this)} />
+      <General field="Name" onSub={this.onSubmit.bind(this)} editMode={this.state.name[1]} name={this.state.name[0]} />
+      <General field="Email" onSub={this.onSubmit.bind(this)} editMode={this.state.email[1]} name={this.state.email[0]}/>
+      <General field="Phone" onSub={this.onSubmit.bind(this)} editMode={this.state.phone[1]} name={this.state.phone[0]}/>
       {/* Live CV preview */}
+      </div>
       <hr></hr>
+      <div className="cvSection">
       <h1>CV Preview:</h1><hr></hr>
       <h1 id="CV_name">Name: {this.state.name}</h1>
       <h1 id="email">Email: {this.state.email}</h1>
       <h1 id="CV_phone">Phone: {this.state.phone}</h1>
-  </div>)
+      </div>
+    </div>)
   }
 }
 
--- src/components/General.js
+++ src/components/General.js
@@ -2,47 +2,32 @@ import React from 'react';
 
 class General extends React.Component{
 
-    constructor() {
-        super();
-    
-        this.state = {            
-            editMode: true     
-          } 
-      }
-
-      toggleEdit()
-      {
-        if (this.state.editMode) {                
-          this.setState({editMode: false})
-        }
-        else{
-          this.setState({editMode: true}
-            )}
-      }
-
-    render(props)
-    {        
-       
-
-        if (this.state.editMode) {
-                return(<div>
+    render()
+    {               
+        if (this.props.editMode) {            
+            return(
+                <div>
                     <form onSubmit={this.props.onSub}>
                     <label>{this.props.field}</label>
                     <input type='text'></input>
                     <button type='submit'>Submit</button>
                     </form>                
                 </div>
-            )}
-            else{
+            )
+            }
+            else{                
                 return(
                     
                     <div>
+                    <form onSubmit={this.props.onSub}>
                     <label>{this.props.field}: {this.props.name}</label>
-                    <button onClick={this.toggleEdit}>Edit</button>                    
+                    <button type='submit'>Edit</button> 
+                    </form>                   
                     </div>
                 )
-            }      
+            }                
     }
+     
 }
 
 export default General;
